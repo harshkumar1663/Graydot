976208127080690193b3b3f71dd30c0181d4de44
harshkumar1663 <harshkumar.s1663@gmail.com>
2023-06-08 16:53:29 +0530

    Added Comments

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
+// function to allow dropping of dragged object
 function allowDrop(event) {
     event.preventDefault();
 }
-
+// function to init dragging of the object
 function drag(event, id) {
     event.dataTransfer.setData("text/plain", id);
     event.target.classList.add("dragging");
 }
-
+// function to handle dropping of the object into the second container
 function drop(event) {
     event.preventDefault();
-    var data = event.dataTransfer.getData("text/plain");
-    var Element = document.getElementById(data);
-    container2.appendChild(Element);
-    Element.classList.remove("dragging");
-    displaySuccessMessage();
+    var data = event.dataTransfer.getData("text/plain"); //getting id of dropped element
+    var Element = document.getElementById(data); //getting element by extracted id
+    container2.appendChild(Element); //appending new element to the 2nd container
+    Element.classList.remove("dragging"); //remove 'dragging' class to remove opacity effect
+    displaySuccessMessage(); 
 }
 
+// function to display success message upon successful drag-and-drop
 function displaySuccessMessage() {
     var successMessage = document.getElementById("successMessage");
     successMessage.textContent = "Item dropped successfully!";
 }
-
+// function to reset both the containers upon pressing of button
 function resetContainers() {
     var container1 = document.getElementById("container1");
     var container2 = document.getElementById("container2");
